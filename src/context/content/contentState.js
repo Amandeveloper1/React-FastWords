3b18d3abda3314102f5c48006ac2bc8d1d2e3d10
Aman <gd15788@gmail.com>
2021-11-02 12:06:11 +0530

    initial changes

--- src/context/content/contentState.js
+++ src/context/content/contentState.js
@@ -5,21 +5,17 @@ const ContentState = (props) => {
 
     const contents = [
         {
-            'sentence':'This is my firt sentence'
+            'sentence':'By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence.'
         },
         {
-            'sentence':'This is my second sentence'
+            'sentence':'simplest sentences in English take the verb be, but be is not the only verb in English. Be is actually a primary auxiliary verb. The vast majority of verbs in English belong to a category called lexical verbs By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence.'
         },
         {
-            'sentence':'This is my third sentence'
+            'sentence':'Note that we cannot use articles before an adjective. However, if the adjective is followed by a singular noun we have to use an article or another determiner By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence.'
         },
         {
-            'sentence':"That is my first name is a hero and what can i do now this is my favirate word is computer."
-        },
-        {
-            'sentence':'This is my forth sentence'
+            'sentence':'A prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence. By adding short prepositional phrases to your sentence, you can tell when or where something happens  prepositional phrase is a group of words that begins with a preposition. The prepositional phrase usually goes at the end of the sentence.'
         }
-       
     ]
 
     return ( < > 
